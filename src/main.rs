eab3b76bfa0d885c3f08ce07eb00541848c4606b
Rate980 <xv0nnj0@icloud.com>
2022-06-23 07:38:49 +0900

    Add first commit

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+const BLACK_CHR: &str = "黒";
+const WHITE_CHR: &str = "白";
+const CLEAR_CHR: &str = "  ";
+
+const TABLE_SIZE: usize = 8;
+
+const CLEAR: u8 = 0;
+const BLACK: u8 = 1;
+const WHITE: u8 = 2;
+
+
+fn print_table(table: &[[u8; TABLE_SIZE]; TABLE_SIZE]){
+    for line in table {
+        for stone in line{
+            let chr = match *stone {
+                CLEAR => CLEAR_CHR,
+                BLACK => BLACK_CHR,
+                WHITE => WHITE_CHR,
+                _ => "",
+            };
+            print!("{}", chr)
+        }
+        println!()
+    }
+}
+
+fn main() {
+    let mut table = [[CLEAR; TABLE_SIZE]; TABLE_SIZE];
+    // 初期位置にコマを置く
+    table[3][3] = WHITE;
+    table[3][4] = BLACK;
+    table[4][3] = BLACK;
+    table[4][4] = WHITE;
+    print_table(&table);
+}
